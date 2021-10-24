a5400a04f7eb329c3b26c7de70026ceede6c927b
Pradeep Raj <60281056+MPradeepRaj02@users.noreply.github.com>
2021-10-24 12:23:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ url = 'https://api.binance.com/api/v1/ticker/24hr?symbol='
 
 def top(i):
     allpairs = pd.DataFrame(client.get_ticker())
-    relev = allpairs[allpairs.symbol.str.contains('USDT')]
+    relev = allpairs[allpairs.symbol.str.contains('USDT')] #Channge Pair as per need
     non_lev = relev[~((relev.symbol.str.contains('UP')) | (relev.symbol.str.contains('DOWN')))]
     top = non_lev.symbol.values[i]
     return top
@@ -18,8 +18,8 @@ def top(i):
 def get_top(n):    
     while True:
         time.sleep(5)
-        if n == 313:
-            n = 0
+        if n == 313: #here 313, because there is only 313 USDT pairs are available for trade in binance 
+            break
         top_asset = top(n)
         response = requests.get(url + top_asset)
         content = response.content.decode("utf8")
@@ -39,7 +39,7 @@ while True:
     cryptoN, percentN, n = get_top(n)
     crypto.append(cryptoN)
     percent.append(percentN)
-    print(percent,crypto)
+    #print(percent,crypto)
     n += 1
     if n == 313:
         coins = [percent,crypto]
